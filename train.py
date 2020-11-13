2cc8358f61e3b44e0de450fa0b978b8be13478d1
step305 <step305@mail.ru>
2020-11-14 02:39:12 +0300

    Changed comments

--- train.py
+++ train.py
@@ -55,37 +55,37 @@ def knn_train(img_dir: str,
     x = []
     y = []
 
-    # Loop through each person in the training set
+    # For each class sub-directory in source directory
     for class_dir in os.listdir(img_dir):
         if not os.path.isdir(os.path.join(img_dir, class_dir)):
             continue
 
-        # Loop through each training image for the current person
-        for img_path in image_files_in_folder(os.path.join(img_dir, class_dir)):
-            image = face_recognition.load_image_file(img_path)
+        # For each image in class sub-directory
+        for image_path in image_files_in_folder(os.path.join(img_dir, class_dir)):
+            image = face_recognition.load_image_file(image_path)
             face_bounding_boxes = face_recognition.face_locations(image)
 
             if not len(face_bounding_boxes) == 1:
-                # If there are no people (or too many people) in a training image, skip the image.
+                # Only one face should be in image, other cases will be skipped
                 if verbose:
-                    print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(
-                        face_bounding_boxes) < 1 else "Found more than one face"))
+                    print("Cannot use {} for training: {}".format(image_path, "no face" if len(
+                        face_bounding_boxes) < 1 else "too many faces"))
             else:
-                # Add face encoding for current image to the training set
+                # Add encoding of the found face to the training set
                 x.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
                 y.append(class_dir)
 
-    # Determine how many neighbors to use for weighting in the KNN classifier
     if n_neighbors is None:
+        # Set default neighbors count to use for weighting in the KNN classifier
         n_neighbors = int(round(math.sqrt(len(x))))
         if verbose:
-            print("Chose n_neighbors automatically:", n_neighbors)
+            print("Default n_neighbors = {} used:".format(n_neighbors))
 
-    # Create and train the KNN classifier
+    # Train the KNN classifier
     knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
     knn_clf.fit(x, y)
 
-    # Save the trained KNN classifier
+    # Store the trained KNN classifier using pickle
     if model_path is not None:
         with open(model_path, 'wb') as f:
             pickle.dump(knn_clf, f)
